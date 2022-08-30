7de4802aec01febdf2612eaaf72704871e671c1e
Nick Martin <111902885+nick-s-martin@users.noreply.github.com>
2022-08-31 00:59:56 +0100

    Configured LevelController
    Now loads next level once all enemies are dead

--- AB Clone/Assets/Scripts/LevelController.cs
+++ AB Clone/Assets/Scripts/LevelController.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class LevelController : MonoBehaviour
 {
@@ -24,6 +25,7 @@ public class LevelController : MonoBehaviour
     void GoToNextLevel()
     {
         Debug.Log("Go to level" + _nextLevelName);
+        SceneManager.LoadScene(_nextLevelName); 
     }
 
     bool MonstersAreAllDead()
