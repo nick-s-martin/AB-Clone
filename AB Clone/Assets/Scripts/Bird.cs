7c07f7f93694cbfe23f06047a1703e8ec2e4199c
Nick Martin <111902885+nick-s-martin@users.noreply.github.com>
2022-08-30 13:52:12 +0100

    Added Bird.cs

---
+++ AB Clone/Assets/Scripts/Bird.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bird : MonoBehaviour
+{
+    [SerializeField] float _launchForce = 500;
+
+    Vector2 _startPosition;
+    Rigidbody2D _rigidbody2D;
+    SpriteRenderer _spriteRenderer;
+
+    void Awake()
+    {
+        _rigidbody2D = GetComponent<Rigidbody2D>();
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _startPosition = _rigidbody2D.position;
+        _rigidbody2D.isKinematic = true;
+    }
+
+    void OnMouseDown()
+    {
+        _spriteRenderer.color = Color.red;
+    }
+
+    void OnMouseUp()
+    {
+        var currentPosition = _rigidbody2D.position;
+        Vector2 direction = _startPosition - currentPosition;
+        direction.Normalize();
+
+        _rigidbody2D.isKinematic = false;
+        _rigidbody2D.AddForce(direction * _launchForce);
+
+        _spriteRenderer.color = Color.white;
+    }
+
+    void OnMouseDrag()
+    {
+        Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        transform.position = new Vector3(mousePosition.x, mousePosition.y, transform.position.z);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
