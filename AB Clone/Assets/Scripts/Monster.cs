122208454d5b7f2d2a78cb7b38ba86716e96bbb6
Nick Martin <111902885+nick-s-martin@users.noreply.github.com>
2022-08-31 00:34:54 +0100

    Changes to Monster.cs
    Made changes to death:
    -Sprite changes
    -Checks if already dead
    -Particle effect

--- AB Clone/Assets/Scripts/Monster.cs
+++ AB Clone/Assets/Scripts/Monster.cs
@@ -6,6 +6,9 @@ using UnityEngine;
 public class Monster : MonoBehaviour
 {
     [SerializeField] Sprite _deadSprite;
+    [SerializeField] ParticleSystem _particleSystem;
+
+    bool _hasDied;
 
     void OnCollisionEnter2D(Collision2D collision)
     {
@@ -17,6 +20,9 @@ public class Monster : MonoBehaviour
 
     bool ShouldDieFromCollision(Collision2D collision)
     {
+        if (_hasDied)
+            return false;
+
         Bird bird = collision.gameObject.GetComponent<Bird>();
         if (bird != null)
             return true;
@@ -29,6 +35,9 @@ public class Monster : MonoBehaviour
 
     void Die()
     {
-        gameObject.SetActive(false);
+        _hasDied = true;
+        GetComponent<SpriteRenderer>().sprite = _deadSprite;
+        _particleSystem.Play();
+        //gameObject.SetActive(false);
     }
 }
